9cafa9ee42c714ddbc98222f1f52aa8894825dba
Justin Aiken <60tonangel@gmail.com>
2018-02-21 14:45:26 -0700

    Fix odd namespacing issue

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,5 +1,5 @@
 module API
-  class SessionsController < ApplicationController
+  class SessionsController < ::API::ApplicationController
     skip_before_action :authenticate_user_from_token!
     before_action :ensure_params_exist
 
